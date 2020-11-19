79d439f821d30915006751ebc1bd5e0d43f35eb8
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-11-19 09:36:22 +0200

    Header and Description layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
+import { Header } from './components/Header';
+import { Description } from './components/Description';
 import './App.scss';
-import { Switch, Link, Route } from 'react-router-dom';
 
 export const App = () => (
-  <div>
-    React starter pack
-    <div>
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-      </nav>
-
-      <Switch>
-        <Route path="/users">
-          <div>Users page</div>
-        </Route>
-        <Route path="/">
-          <div>Home page</div>
-        </Route>
-      </Switch>
-    </div>
-  </div>
+  <main className="app">
+    <Header />
+    <Description />
+  </main>
 );
